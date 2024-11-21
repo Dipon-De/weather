0f3754512b011a9281586571c3a70feffc55814d
Dipon-De <diponde26092004@gmail.com>
2024-11-21 23:04:37 +0530

    8th

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// const apiKey = config.apiKey;                // API key from config.js      for locally run 
-
-// const apiKey = process.env.NEXT_PUBLIC_API_KEY; // api key for deployment     for public 
-
-const apiKey = API_KEY;
+const apiKey = config.apiKey; // API key from config.js      
 
 const weatherDataEle = document.querySelector(".weather-data");
 const cityNameEle = document.querySelector("#city-name");
